93269a7f37a9d58cd50e0ef405553c9eca139f28
Serge <you@example.com>
2014-11-10 00:02:54 +0300

    experiment

--- application.js
+++ application.js
@@ -3,6 +3,177 @@ executeFile('custom/models/room.js');
 executeFile('custom/models/probki.js');
 
 var home = new Home({name: 'Дом'});
+function Room(settings) {
+    var room = this;
+
+    if (typeof(settings) !== 'object') {
+        settings = {};
+    }
+
+    room.name = settings.name;
+    room.lamp = settings.lamp;
+    room.motionSensor = settings.motionSensor;
+    room.luxSensor = settings.luxSensor;
+    room.temperatureSensor = settings.temperatureSensor;
+    room.switcher = settings.switcher;
+
+    room.minLux = settings.minLux;
+    room.emptyRoomTimeout = settings.timeout;
+
+    //Defaults
+
+    room.illuminationIsOn = false;
+    room.currentLux = 0;
+    room.currentTemperature = 0;
+    room.isEmpty = true;
+    room.motionIsNear = false;
+    room.isDark = true;
+    room.lastMotionAt = new Date(1);
+    //Lamp
+
+
+    //init room
+
+    console.log(room.name + ': Инициализирую помещение');
+    room.turnOffLamp();
+    room.subscribeToMotionSensor();
+    room.subscribeToLuxSensor();
+    room.subscribeToTemperatureSensor();
+    //room.subscribeToSwitcher();
+
+
+}
+
+Room.prototype.illuminate = function () {
+    console.log(this.name + ': Включаю свет');
+    this.lamp.Set(1);
+    this.illuminationIsOn = true;
+};
+
+Room.prototype.turnOffLamp = function () {
+    console.log(this.name + ': Выключаю свет');
+    this.lamp.Set(0);
+    this.illuminationIsOn = false;
+    this.temporaryIlluminationIsOn = false;
+};
+
+//Motion
+
+Room.prototype.subscribeToMotionSensor = function () {
+    console.log(this.name + ': Подписываюсь на датчик движения');
+    if (this.motionSensor) {
+        this.motionSensor.bind(this.onMotionDetect);
+    }
+};
+
+Room.prototype.onMotionDetect = function () {
+    console.log(this.name + ': Получена информация от датчика движения');
+    if (this.emptyRoomTimer) {
+        clearTimeout(this.emptyRoomTimer);
+    }
+    if (this.value) {
+        console.log(this.name + ': Есть движение в комнате');
+        this.isEmpty = false;
+        this.lastMotionAt = new Date();
+    } else {
+        var _this = this;
+        this.emptyRoomTimer = setTimeout(function () {
+            console.log(_this.name + ': Нет движения в комнате');
+            _this.isEmpty = true;
+            _this.onChangesDetect();
+        }, this.emptyRoomTimeout * 1000)
+    }
+    this.onChangesDetect();
+};
+
+//Lux
+
+Room.prototype.onLuxChange = function () {
+    if (!this.illuminationIsOn && !this.temporaryIlluminationIsOn) {
+        this.currentLux = this.value;
+        this.isDark = (this.currentLux < this.minLux);
+        console.log(this.name + ': Изменилась освещенность');
+    }
+    this.onChangesDetect();
+};
+
+Room.prototype.subscribeToLuxSensor = function () {
+    if (this.luxSensor) {
+        console.log(this.name + ': Подписываюсь на датчик освещенности');
+        this.luxSensor.bind(this.onLuxChange);
+    }
+};
+
+// Switcher
+
+Room.prototype.onSwitcherChange = function () {
+    console.log(this.name + ': Выключатель был нажат');
+
+    this.onChangesDetect();
+};
+
+Room.prototype.subscribeToSwitcher = function () {
+    if (this.switcher) {
+        console.log(this.name + ': Подписываюсь на выключатель');
+        this.switcher.bind(this.onSwitcherChange);
+    }
+};
+
+
+//Temperature
+
+Room.prototype.onTemperatureChange = function () {
+    console.log(this.name + ': Изменилась температура');
+    this.currentTemperature = this.value;
+    this.onChangesDetect();
+};
+
+Room.prototype.subscribeToTemperatureSensor = function () {
+    if (this.temperatureSensor) {
+        console.log(this.name + ': Подписываюсь на датчик температуры');
+        this.temperatureSensor.bind(this.onTemperatureChange);
+    }
+};
+
+//Motion near
+
+Room.prototype.onMotionNear = function () {
+    console.log(this.name + ': Движение в помещении рядом');
+    this.motionIsNear = true;
+    this.onChangesDetect();
+    this.motionIsNear = false;
+};
+
+//-------------------------------------------------------
+
+Room.prototype.onChangesDetect = function () {
+    console.log(this.name + ': Детектированы изменеия');
+    if (!this.isEmpty && !this.illuminationIsOn && this.isDark) {
+
+        this.illuminate();
+        if (this.neighbors) {
+            this.neighbors.forEach(function (neighbor) {
+                neighbor.onMotionNear();
+            });
+        }
+    }
+    if (this.isEmpty && this.illuminationIsOn) {
+        this.turnOffLamp();
+    }
+    if (this.motionIsNear) {
+        if (!this.illuminationIsOn && this.isDark) {
+            var _this = this;
+            this.illuminate();
+            setTimeout(function () {
+                if (_this.isEmpty) {
+                    _this.turnOffLamp();
+                }
+            }, 20 * 1000);
+        }
+
+    }
+
+};
 
 var kitchen = new Room(
     {
