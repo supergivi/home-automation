62db5ddbda400bb0731b9dd10901d618ebef9d2f
supergivi <chazoff@gmail.com>
2016-11-06 15:06:23 +0300

    s

--- models/home.js
+++ models/home.js
@@ -54,12 +54,16 @@ var Home = function (settings) {
     home.onInHomeSwitcherChange = function (val) {
         if (home.full) {
             home.full = false;
+            console.log(home.name + ': emptyyy');
+
             //home.emptyTimeout = setTimeout(function () {
             home.rooms.forEach(function (room) {
                 room.forceEmpty();
             });
             //}, 120 * 1000)
         } else {
+            console.log(home.name + ': fullll');
+
             home.full = true;
             home.rooms.forEach(function (room) {
                 room.disableForceEmpty();
