5b8c3fb1c2469215eda609fe6449db0a258f00a7
supergivi <chazoff@gmail.com>
2020-09-04 00:24:29 +0300

    s

--- models/rc.js
+++ models/rc.js
@@ -3,6 +3,7 @@ var Rc = function () {
 
   self.start = function () {
     zway.devices[13].instances[0].commandClasses[91].data.currentScene.bind(function () {
+      console.log("devices.13 " + zway.devices[13].instances[0].commandClasses[91].data.keyAttribute.value)
       if (this.value === 4 && zway.devices[13].instances[0].commandClasses[91].data.keyAttribute.value === 0) {
         if (self.toggle4) {
           zway.devices[11].instances[1].SwitchBinary.Set(0)
